29ebe60b8ee2fb89f99785eec2b7b015cc4036ac
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-04-27 22:16:02 +1000

    second class file

---
+++ array declare.java
@@ -0,0 +1,8 @@
+public class arraydeclare {
+    public static void main(String[] args){
+        int a[] = {33,44,55,66,77,88};
+        //declare the array
+        for(int=0; i<a.length; i++);
+        System.out.println("a[i]");
+    }
+}
