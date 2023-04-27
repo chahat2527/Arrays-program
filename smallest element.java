20c6be0f516b5b56516c1a14afeb1c1f185bec03
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-04-27 22:24:42 +1000

    Third file

---
+++ smallest element.java
@@ -0,0 +1,13 @@
+public class smallest array{
+
+    public static void main(String[] args){
+        //initialize array
+        initialize arr[] = {1,2,3,4,5};
+        int sum=0;
+        //Loop through calculate the array
+        for(int i=0; i< a.length; i++);{
+            Sum=Sum arr[i];
+        }
+        System.out.println("Sum of all the elements in array");
+    }
+}
